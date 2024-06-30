92abdaec1c0cfd0db345a2f0d8bab861bc97a3f7
Md. Kabirul Hossain <mdkabirulhossainj@gmail.com>
2024-07-01 01:57:55 +0600

    In Home Banner section Done

--- src/Routes/Routes.jsx
+++ src/Routes/Routes.jsx
@@ -2,6 +2,7 @@ import {
     createBrowserRouter,
   } from "react-router-dom";
 import Main from "../Layout/Main";
+import Home from "../Pages/Home/Home/Home";
 
 const router = createBrowserRouter([
     {
@@ -10,8 +11,8 @@ const router = createBrowserRouter([
       children:[
         {
             path:"/",
-            
-        }
+            element:<Home></Home>  
+        },
       ]
     },
   ]);
