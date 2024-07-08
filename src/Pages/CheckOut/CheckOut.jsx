2060faea713f6f28f03cc321da56ab9246d9fabf
Md. Kabirul Hossain <mdkabirulhossainj@gmail.com>
2024-07-09 02:47:17 +0600

    Complete checkout page

---
+++ src/Pages/CheckOut/CheckOut.jsx
@@ -0,0 +1,17 @@
+import checkout_img from '../../assets/images/checkout/checkout.png'
+const CheckOut = () => {
+    return (
+        <div>
+            <div>
+            <img className='w-full' src={checkout_img} alt="" />
+            </div>
+            <div>
+                
+            </div>
+
+            
+        </div>
+    );
+};
+
+export default CheckOut;
