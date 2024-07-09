5f86435e93c777a3183080fe7b652b24fa563955
Md. Kabirul Hossain <mdkabirulhossainj@gmail.com>
2024-07-09 18:12:40 +0600

    Completed Service section

--- src/Pages/Home/Services/ServiceCard.jsx
+++ src/Pages/Home/Services/ServiceCard.jsx
@@ -1,7 +1,8 @@
 
 import { FaArrowRight } from "react-icons/fa";
+import { Link } from "react-router-dom";
 const ServiceCard = ({service}) => {
-    const{title, img, price} = service;
+    const{_id, title, img, price} = service;
     return (
         <div className="card bg-base-100 w-96 shadow-xl">
             <figure className="px-10 pt-10">
@@ -14,7 +15,10 @@ const ServiceCard = ({service}) => {
                 <h2 className="card-title">{title}</h2>
                 <div className="flex justify-between">
                     <p>{price}</p>
-                    <FaArrowRight></FaArrowRight>
+                    <Link to={`/checkout/${_id}`}>
+                    <button><FaArrowRight></FaArrowRight></button>
+                    </Link>
+                    
                 </div>
             </div>
         </div>
